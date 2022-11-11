a1075acb4cda86672c0c35a565087e52223586ff
mark <1210917553@qq.com>
2022-11-11 16:08:36 +0800

    chore: update export

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-/**
- * 返回 hello 开头的字符串
- * @param name - input string
- * @returns 'hello xxx'
- * @example
- * ```ts
- * sayHello('utils') => 'hello utils'
- * ```
- * @beta
- * @author Mark Peng
- */
-export function sayHello(name: string): string {
-  return `hello ${name}`
-}
+export * from './is'
+export * from './object'
+export * from './array'
+export * from './time'
